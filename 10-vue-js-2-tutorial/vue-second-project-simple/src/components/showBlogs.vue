b49485098441db2d38ad1e62ded89dc65d1d1b3e
Kioh-Cna <116043570+Kioh-R@users.noreply.github.com>
2022-10-30 22:11:35 +0900

    refs #14

---
+++ 10-vue-js-2-tutorial/vue-second-project-simple/src/components/showBlogs.vue
@@ -0,0 +1,80 @@
+<template>
+  <div v-theme:column="'narrow'" id="show-blogs">
+    <h1>All Blog Articles</h1>
+    <input type="text" v-model="search" placeholder="search blogs" />
+    <div class="single-blog" v-for="blog in filteredBlogs" v-bind:key="blog.id">
+      <router-link v-bind:to="'/blog/' + blog.id"
+        ><h2 rainbow>{{ blog.title | toUppercase }}</h2></router-link
+      >
+      <article>{{ blog.body | snippet }}</article>
+    </div>
+  </div>
+</template>
+<script>
+import searchMixin from "../mixins/searchMixin";
+
+export default {
+  data() {
+    return {
+      blogs: [],
+      search: "",
+    };
+  },
+  methods: {},
+  created() {
+    this.$http.get("http://jsonplaceholder.typicode.com/posts").then((data) => {
+      console.log(
+        "🚀 ~ file: showBlogs.vue ~ line 17 ~ this.$http ~ data",
+        data
+      );
+      // this body is not object's body, it is response's body.
+      this.blogs = data.body.slice(0, 10);
+    });
+    // this.$http
+    //   .get("http://jsonplaceholder.typicode.com/posts")
+    //   .then(function (data) {
+    //     console.log(
+    //       "🚀 ~ file: showBlogs.vue ~ line 17 ~ this.$http ~ data",
+    //       data
+    //     );
+    //     this.blogs = data.body.slice(0, 10);
+    //   });
+  },
+  computed: {
+    // filteredBlogs: function () {
+    //   return this.blogs.filter((blog) => {
+    //     return blog.title.includes(this.search);
+    //   });
+    // },
+  },
+  filters: {
+    /*'to-uppercase': function(value){
+            return value.toUpperCase();
+        }*/
+    toUppercase(value) {
+      return value.toUpperCase();
+    },
+  },
+  directives: {
+    // rainbow: {
+    //   // eslint-disable-next-line no-unused-vars
+    //   bind(el, binding, vnode) {
+    //     el.style.color = "#" + Math.random().toString(16).slice(2, 8);
+    //   },
+    // },
+  },
+  mixins: [searchMixin],
+};
+</script>
+<style>
+#show-blogs {
+  max-width: 800px;
+  margin: 0, auto;
+}
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
+}
+</style>
--- 10-vue-js-2-tutorial/vuejs-playlist/app.js
+++ 10-vue-js-2-tutorial/vuejs-playlist/app.js
@@ -1,14 +1,6 @@
 new Vue({
-  el: "#vue-app",
+  el: "#app",
   data: {
-    name: "Shaun",
-    job: "Ninja",
-    website: "http://www.yahoo.co.jp",
-    websiteTag: '<a href="http://www.yahoo.co.jp">Yahoo!!!!!</a>',
-  },
-  methods: {
-    greet: function (time) {
-      return "Good " + time + " " + this.name;
-    },
+    test: "test value",
   },
 });
